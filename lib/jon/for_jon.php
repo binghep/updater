006a33b719dde4706d81e5f0eb84c77a2ac8eed7
root <root@debian>
2016-06-10 21:18:33 +0000

    lala

--- lib/jon/for_jon.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require '/usr/share/nginx/www/ipzmall.com/wp/wp-content/plugins/datafeedr-api/libraries/datafeedr.php';
-
-$datafeedrApi=new DatafeedrApi( 'uqzonmoujxgycslwwg9c', 'IAgLDmR00vwERMpewaALXOkvKGcpXHEFHTl0nxLGDScUGimhmgRkoILoIFc97geu' );
-
